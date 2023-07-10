dffe31dc077a3d728a72978eb136432e58a60cb9
Patrick762 <lutzpatti86@gmail.com>
2023-07-10 17:08:05 +0200

    updated import for texts

--- import.sh
+++ import.sh
@@ -15,8 +15,17 @@ rm -r ./custom_components/streamdeck
 # Insert new version
 cp -R ./ha-core/homeassistant/components/streamdeck ./custom_components/streamdeck
 
+# Create translations directory if not existing
+mkdir -p ./custom_components/streamdeck/translations
+
 # Remove manifest.json
 rm ./custom_components/streamdeck/manifest.json
 
+# Remove en.json
+rm ./custom_components/streamdeck/translations/en.json
+
 # Insert current manifest.json
 cp ./manifest_template.json ./custom_components/streamdeck/manifest.json
+
+# Insert en.json translation
+cp ./translations_en.json ./custom_components/streamdeck/translations/en.json
